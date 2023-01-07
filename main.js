297d0b88279b2bdf27cf5dfd8008b2c88b7b6b04
Andybf <anderson584bf@gmail.com>
2023-01-07 20:05:43 -0300

    func: fisrt try of IA

--- main.js
+++ main.js
@@ -26,10 +26,10 @@ class Canvas {
 
     drawCircle() {
         this.clear();
-        let x = randBetween(8,this.canvas.width-8,0);
-        let y = randBetween(8,this.canvas.width-8,0);
-        let radiusX = randBetween(4,this.canvas.width/4,0);
-        let radiusY = randBetween(4,this.canvas.width/4,0);
+        let x = randBetween(16,this.canvas.width/2,0);
+        let y = randBetween(16,this.canvas.width/2,0);
+        let radiusX = randBetween(8,this.canvas.width/2,0);
+        let radiusY = randBetween(8,this.canvas.width/2,0);
         let rotation = Math.PI;
         let startAngle = 0;
         let endAngle = 2*Math.PI;
@@ -45,8 +45,8 @@ class Canvas {
         this.clear();
         let x = randBetween(1,16,0);
         let y = randBetween(1,16,0);
-        let w = randBetween(1,16,0);
-        let h = randBetween(1,16,0);
+        let w = randBetween(12,16,0);
+        let h = randBetween(12,16,0);
         this.context.fillStyle = 'black';
         this.context.fillRect(x,y,w,h);
     }
@@ -55,7 +55,10 @@ class Canvas {
         let x = 0
         let y = 0;
         brain.forEach( neuron => {
-            this.context.fillStyle = `hsl(180, 80%, ${neuron.connectionWeight*255}%)`;
+            // if (neuron.connectionWeight*100/255 > 0) {
+            //     debugger;
+            // }
+            this.context.fillStyle = `hsl(${180-neuron.connectionWeight*100/360}, 80%, 50%)`;
             this.context.fillRect(x,y,1,1);
             x++;
             if (x == 32) {
@@ -84,14 +87,17 @@ class Perceptron {
     constructor() {
         this.receivedImageValue = 0;
         this.realImageValue = 0;
+        for (let x=0; x<1024; x++) {
+            this.brain.push({
+                light : 0,
+                connectionWeight : 0
+            });
+        }
     }
 
     recieveImage(lightArray) {
-        lightArray.forEach( lightPoint => {
-            this.brain.push({
-                light : lightPoint,
-                connectionWeight : 0
-            });
+        lightArray.forEach( (lightPoint, index) => {
+            this.brain[index].light = lightPoint;
         });
     }
 
@@ -99,27 +105,31 @@ class Perceptron {
         this.brain.forEach( neuron => {
             this.receivedImageValue += neuron.light * neuron.connectionWeight
         });
-        if (this.receivedImageValue >= this.realImageValue) {
+        if (this.receivedImageValue > this.realImageValue) {
             console.log('detected!');
             this.brain.forEach( neuron => {
-                this.connectionWeight += neuron.light/255;
+                neuron.connectionWeight += neuron.light/360;
             });
             this.realImageValue = this.receivedImageValue;
         } else {
-            console.log('Not detected');
             this.brain.forEach( neuron => {
-                this.connectionWeight -= neuron.light/255;
+                neuron.connectionWeight -= neuron.light/360;
             });
         }
+        this.receivedImageValue = 0;
     }
 }
 
+let inputCanvas;
+let outputCanvas;
+let perceptron;
+
 function randBetween(minSize, maxSize, decimals) {
     return Number((Math.random() * (maxSize - minSize) + minSize).toFixed(decimals));
-}   
+}
 
 function main() {
-
+    let intervalId;
     window.addEventListener('keyup', event => {
         switch(event.key) {
             case 'r':
@@ -127,18 +137,35 @@ function main() {
                 break;
             case 'c':
                 inputCanvas.drawCircle();
+                break;
+            case 't':
+                intervalId = setInterval(() => {
+                    if(randBetween(1,2,0)>1) {
+                        inputCanvas.drawCircle();
+                    } else {
+                        inputCanvas.drawRect();
+                    }
+                    action();
+                },1);
+                break;
+            case 'p':
+                clearInterval(intervalId);
         }
-        perceptron.recieveImage(inputCanvas.exportCanvasImage());
-        outputCanvas.drawLightData(perceptron.brain);
     });
 
-    let inputCanvas = new Canvas('#inputCanvas','#E0E8F6');
-    let outputCanvas = new Canvas('#outputCanvas','#F6E8E0');
+    inputCanvas = new Canvas('#inputCanvas','#E0E8F6');
+    outputCanvas = new Canvas('#outputCanvas','#F6E8E0');
 
     let circleValue = 0;
     let rectValue = 0;
 
-    let perceptron = new Perceptron();
+    perceptron = new Perceptron();
+}
+
+function action() {
+    perceptron.recieveImage(inputCanvas.exportCanvasImage());
+    perceptron.guessWhatImageIs();
+    outputCanvas.drawLightData(perceptron.brain);
 }
 
 
